a5053c06c30293b9c717879ddfd487277aa96979
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-10-18 17:55:29 +0200

    Final main.js commit

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const path = require("path");
 const readline = require("readline");
 
 var pathToEdit = path.join(__dirname + "/../");
-var dateLength = 25;
+
+
 
 fs.readdir(pathToEdit, (err, files) => {
     files.forEach(file => {
+      var dateVal = null;
+      var title = null;
         //Go through each and every html file:
         //Check if the file is an html file
         if(file.split(".")[1] == "html") {
@@ -18,16 +21,21 @@ fs.readdir(pathToEdit, (err, files) => {
             if(line.split("property=").length > 1) {
             if(line.split("property=")[1].substring(1,26) == "og:article:published_time") {
               //Get the modified date
-              var date = line.split("content=")[1].substring(1,11)
-              console.log(date);
-              fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + date + ".html", (err) => {
-                if(err) throw err;
-
-              });
+              dateVal = line.split("content=")[1].substring(1,11).split("-");
             }
             
           }
-            
+          if(line.split("<title>").length > 1) {
+            title = line.split("<title>")[1].substring(0, line.split("<title>")[1].length - 8)
+          }
+
+          if(dateVal != null && title != null) {
+            fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + title + "_" + dateVal[0] + dateVal[1] + dateVal[2] + ".html", (err) => {
+                if(err) throw err;
+
+              });  
+          }
+           
             
         });
         }
