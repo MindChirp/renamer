2d4fc0cec884c217bb06c076bdb852e02527c0c9
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-10-18 18:10:47 +0200

    Final commit

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ const readline = require("readline");
 var pathToEdit = path.join(__dirname + "/../");
 
 
-
 fs.readdir(pathToEdit, (err, files) => {
+  if(err) throw err;
+  console.log("Renaming files, please wait...");
     files.forEach(file => {
-      var dateVal = null;
-      var title = null;
+
+
+ 
         //Go through each and every html file:
         //Check if the file is an html file
         if(file.split(".")[1] == "html") {
+          var dateVal = null;
+          var title = null;
           var readInterface = readline.createInterface({
             input: fs.createReadStream(pathToEdit + "/" + file),
             console: false
@@ -29,15 +33,18 @@ fs.readdir(pathToEdit, (err, files) => {
             title = line.split("<title>")[1].substring(0, line.split("<title>")[1].length - 8)
           }
 
-          if(dateVal != null && title != null) {
-            fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + title + "_" + dateVal[0] + dateVal[1] + dateVal[2] + ".html", (err) => {
-                if(err) throw err;
-
-              });  
-          }
-           
-            
+          
+          
         });
+        setTimeout(function() {
+        if(dateVal != null && title != null) {
+          fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + title + "_" + dateVal[0] + dateVal[1] + dateVal[2] + ".html", (err) => {
+              if(err) throw err;
+            });  
         }
+        }, 100);
+        
+      }
     });
+    console.log("Done!");
   });
