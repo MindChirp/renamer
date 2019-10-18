9e5a11d719522cc0ac5aabd738235cd564dd20d7
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-10-18 16:56:30 +0200

    Third commit

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 const fs = require("file-system");
+const path = require("path");
+const readline = require("readline");
+
+var pathToEdit = path.join(__dirname + "/../");
+
+fs.readdir(pathToEdit, (err, files) => {
+    files.forEach(file => {
+        //Go through each and every html file:
+        //Check if the file is an html file
+        if(file.split(".")[1] == "html") {
+          console.log(file)
+          var readInterface = readline.createInterface({
+            input: fs.createReadStream(pathToEdit + "/" + file),
+            output: process.stdout,
+            console: false
+          });
+
+          readInterface.on('line', function(line) {
+            console.log(line);
+        });
+        }
+    });
+  });
