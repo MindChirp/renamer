c5b74b8c8e94e03c77a37ef11840c8f2b40fd492
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-10-18 17:39:33 +0200

    now has the ability to change file names

--- main.js
+++ main.js
@@ -10,22 +10,26 @@ fs.readdir(pathToEdit, (err, files) => {
         //Go through each and every html file:
         //Check if the file is an html file
         if(file.split(".")[1] == "html") {
-          console.log(file)
           var readInterface = readline.createInterface({
             input: fs.createReadStream(pathToEdit + "/" + file),
-            output: process.stdout,
             console: false
           });
-
           readInterface.on('line', function(line) {
-            if(line.split("property=")[0].substring(1,25) == "og:article:published_time") {
-              console.log(line);
-              console.log(line.split("content=")[0]);
-              line.split("content=")[0].substring(1,10);
+            if(line.split("property=").length > 1) {
+            if(line.split("property=")[1].substring(1,26) == "og:article:published_time") {
+              //Get the modified date
+              var date = line.split("content=")[1].substring(1,11)
+              console.log(date);
+              fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + date + ".html", (err) => {
+                if(err) throw err;
+
+              });
             }
+            
+          }
+            
+            
         });
         }
     });
   });
-
-
